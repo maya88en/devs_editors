7a59c596917d45bab53bd4a93522df2b75abb597
maya88en <maya88en@gmail.com>
2025-10-05 22:47:12 +0300

    add introduction on login page

--- devabollators-2/app/login/page.tsx
+++ devabollators-2/app/login/page.tsx
@@ -39,9 +39,28 @@ const LoginPage = () => {
   return (
     <form
       onSubmit={authenticateUser}
-      className='flex flex-col gap-3 justify-center items-center h-screen max-w-lg mx-auto'
-    >
-      <label>Login with magic link 🧙🏽‍♂️</label>
+      className=' justify-center items-center h-screen max-w-lg mx-auto'
+    > 
+    <div className="text-center"> 
+      {/* <label>Login with magic link 🧙🏽‍♂️</label> */}
+      <h2 className="mt-7"><b>اتعلم مجانا تبني مشاريع مع فريق</b></h2>
+      <h2><b>React.js</b></h2>
+      <h2><b>وتضيفها للبورتفوليو</b></h2>
+      <div className="text-green-900">
+      <h2 className= "mt-2"><b>Ücretsiz Bir ekip ve bir akıl hocasıyla React.js yolculuğunuza başlayın ve yüksek ücretli işlere başvurmak için bir özgeçmiş portföyü oluşturun</b></h2>
+      <h2><b>Diğer geliştiricilerle bağlantı kurun....</b></h2>
+      <h2><b>React.js Projelerinin Ücretsiz Yapılandırılması</b></h2>
+      </div>
+      <h2 className="mt-2"><b>Free Start your React.js journey with a team and a mentor to build a portfolio for CV to apply for high paying jobs</b></h2>
+      <h2><b>Connect with other developers....</b></h2>
+      <h2><b>Free build of React.js Projects</b></h2>
+      <h1 className= " text-red-600 text-5xl mt-2 mb-2"><b>DEVABOLLATORS</b></h1>
+      <div className=" text-blue-950">
+      <h2><b>Verbinden mit Deutsch Developers...</b></h2>
+      <h2><b>kostenlos bauen..... Projektentwicklung mit React.js </b></h2>
+      <h2 className="mb-5"><b>Beginnen Sie Ihre React.js-Reise kostenlos mit einem Team und einem Mentor, um ein Portfolio für Ihren Lebenslauf zu erstellen, mit dem Sie sich auf gut bezahlte Jobs bewerben können.</b></h2>
+      </div>
+      </div>
       <input
         type='text'
         placeholder='Enter email address'
@@ -52,14 +71,14 @@ const LoginPage = () => {
 
       <button
         type='submit'
-        className='px-3 py-2 bg-slate-900 text-white rounded-lg text-base w-full'
+        className='px-3 py-2 bg-slate-900 text-white rounded-lg text-base w-full mb-7'
         disabled={isTokenSent || isLoading}
       >
         {isTokenSent
           ? "Token sent...please check your email address"
           : isLoading
           ? "One moment please..."
-          : "Send magic link"}
+          : "Send link, Link senden, Bağlantıyı Gönder, إرسال الرابط"}
       </button>
     </form>
   );
